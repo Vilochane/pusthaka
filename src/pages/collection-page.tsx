7459fd3385c4d5ededf73f9fbf783d6cb6b6f671
Vajiramal Vilochane <vilochane@gmail.com>
2019-09-07 09:55:20 +0530

    adding text layer

--- src/pages/collection-page.tsx
+++ src/pages/collection-page.tsx
@@ -4,7 +4,7 @@ import PDFViewer from "../components/pdf-viewer";
 
 // eslint-disable-next-line no-unused-vars
 function CollectionPage(props: RouterProps): JSX.Element {
-  return <PDFViewer src="file:///home/sanath/.config/pusthaka/books/16cbc6e92ed.a8b3128527c55c2efa1314d23e8690d12ae28a45.pdf"/>;
+  return <PDFViewer src="file:/home/vilochane/Projects/pusthaka/books/test3.pdf"/>;
 }
 
 export default CollectionPage;
